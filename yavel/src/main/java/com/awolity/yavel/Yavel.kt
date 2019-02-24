93e28bb086be06900cd0411afb033aab6b8b895a
vokod <vokoda@gmail.com>
2019-02-24 22:54:29 +0100

    changed yavel get api to return nonnull values

--- yavel/src/main/java/com/awolity/yavel/Yavel.kt
+++ yavel/src/main/java/com/awolity/yavel/Yavel.kt
@@ -85,15 +85,15 @@ private constructor(keyAlias: String) {
         private var yavels: MutableMap<String, Yavel>? = null
 
         @Throws(YavelException::class)
-        operator fun get(keyAlias: String): Yavel? {
+        operator fun get(keyAlias: String): Yavel {
             if (yavels == null) { // if no yavels were created
                 yavels = HashMap(1)
             }
             return if (yavels!!.containsKey(keyAlias)) { // if the yavel in question is in yavels
-                yavels!![keyAlias]
+                yavels!![keyAlias]!!
             } else { // if the yapel in question is NOT in the yavels
                 yavels!![keyAlias] = Yavel(keyAlias)
-                yavels!![keyAlias]
+                yavels!![keyAlias]!!
             }
         }
     }
